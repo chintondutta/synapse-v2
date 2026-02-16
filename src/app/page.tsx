eb900193c7934508e6a1e94bb5c50b8f255fcd89
chintondutta <chintondutta.tech@gmail.com>
2026-02-16 21:29:43 +0530

    02: database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import {Button} from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-const Page = () => {
-  return (
+const Page = async () => {
+   const users = await prisma.user.findMany();
+
+   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <Button>
-          Click me
-      </Button>
+        {JSON.stringify(users)}
     </div>
-  );
+   );
 };
 
-export default Page
+export default Page;
